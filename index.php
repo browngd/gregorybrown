a3b78c5e407b5b566dfb013f0bb2507f5dea196f
browngd <browngd@gmail.com>
2013-06-03 20:03:18 -0400

    Rename test.php to index.php
    Saved old index.php as index.php.bk.

--- index.php
+++ index.php
@@ -1,18 +1,310 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>BitCrunched!</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Full service Computer and Technology Solutions" content="">
+    <meta name="BitCrunched!" content="">
+    <link href="css/new.css" rel="stylesheet">
+  </head>
+  <body>
+    <!-- navbar -->
+    <div class="navbar navbar-fixed-top" style="position: fixed">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="#"><span style="color: #889D07 !important">Bit</span>Crunched!</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav pull-right">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Services <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li class="nav-header">Residential</li>
+                  <li><a href="#">Web Design</a></li>
+                  <li><a href="#">Computer Repair</a></li>
+                  <li><a href="#">Computer Upgrades</a></li>
+                  <li><a href="#">Virus Removal</a></li>
+                  <li><a href="#">Networking</a></li>
+                  <li><a href="#">Network Security</a></li>
+                  <li><a href="#">Remote Support</a></li>
+                  <li class="divider"></li>
+                  <li class="nav-header">Additional Commercial Services</li>
+                  <li><a href="#">IT Auditing</a></li>
+                  <li><a href="#">IT Consulting</a></li>
+                </ul>
+              </li>
+              <li><a href="#about">Blog</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div><!-- /.navbar-inner -->
+    </div>
+    <div id="top-shadow"></div>
+    <!-- .navbar -->
+    <!-- main content -->
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+    <div class="content">
+      <!-- flex slider -->
+      <div class="flexslider slider">
+        <ul class="slides">
+          <li>
+            <p><br /><br /></p>
+            <p>
+              <h1>Get <span style="color: #889D07 !important; text-shadow: rgba(0,0,0,0.8) 0px 1px 1px">Bit</span><span style="color: #FA6801 !important; text-shadow: rgba(0,0,0,0.8) 0px 1px 1px">Crunched!</span></h1>
+            </p>
+            <p><br /></p>
+            <p>
+              <h3>Full service computer and technology solutions.</h3>
+            </p>
+            <p><br /><br /></p>
+          </li>
+          <li>
+            <p><br /><br /></p>
+            <p>
+              <h1>On-Site Service!</h1>
+            </p>
+            <p><br /></p>
+            <p>
+              <h3>Reliable and Dependable!</h3>
+            </p>
+            <p><br /><br /></p>
+          </li>
+        </ul>
+      </div>
+      <!-- /.flex slider -->
+      <!-- services -->
+      <div class="container boxes">
+          <a href="#">
+            <div class="green-box float-left column1">
+              <div class="box-inner">
+                <div class="icon-orange">
+                  <i class="icon-html5 icon-4x"></i>
+                  <p><h3>Web Design</h3></p>
+                </div>
+              </div>
+            </div>
+          </a>
+          <a href="#">
+            <div class="orange-box float-left column2">
+              <div class="box-inner">
+                <div class="icon-green">
+                  <i class="icon-wrench icon-4x"></i>
+                  <p><h3>Computer Repair</h3></p>
+                </div>
+              </div>
+            </div>
+          </a>
+          <a href="#">
+            <div class="green-box float-left column3">
+              <div class="icon-orange">
+                <i class="icon-arrow-up icon-4x"></i>
+                <p><h3>Computer Upgrades</h3></p>
+              </div>
+            </div>
+          </a>
+          <a href="#">
+            <div class="orange-box float-left column4">
+              <div class="icon-green">
+                <i class="icon-medkit icon-4x"></i>
+                <p><h3>Virus Removal</h3></p>
+              </div>
+            </div>
+          </a>
+          <a href="#">
+            <div class="orange-box float-left column1">
+              <div class="icon-green">
+                <i class="icon-sitemap icon-4x"></i>
+                <p><h3>Networking</h3></p>
+              </div>
+            </div>
+          </a>
+          <a href="#">
+            <div class="green-box float-left column2">
+              <div class="icon-orange">
+                <i class="icon-lock icon-4x"></i>
+                <p><h3>Network Security</h3></p>
+              </div>
+            </div>
+          </a>
+          <a href="#">
+            <div class="orange-box float-left column3">
+              <div class="icon-green">
+                <i class="icon-phone icon-4x"></i>
+                <p><h3>Remote Support</h3></p>
+              </div>
+            </div>
+          </a>
+          <a href="#">
+            <div class="green-box float-left column4">
+              <div class="icon-orange">
+                <i class="icon-group icon-4x"></i>
+                <p><h3>IT Consulting</h3></p>
+              </div>
+            </div>
+          </a>
+        </div>
+<!-- ./services -->
+      <!-- features -->
+      <div class="container">
+        <hr class="feature-divider">
+        <div class="feature">
+          <h2 class="feature-heading">Get&nbsp;<span class="muted">Technology!</span></h2>
+          <p class="lead">If technology is not your thing, leave all the bits to us and Get <span style="color: #889D07 !important; text-shadow: rgba(0,0,0,0.8) 0px 1px 1px">Bit</span><span style="color: #FA6801 !important; text-shadow: rgba(0,0,0,0.8) 0px 1px 1px">Crunched!</span></p>
+        </div>
+        <hr class="feature-divider">
 
-/** Loads the WordPress Environment and Template */
-require('./blog/wp-blog-header.php');
-require('header.php');
+
+      </div>
+      <!-- ./ features -->
+    </div>
+      <footer>
+    <div class="container">
+      <div class="row">
+        <div class="span3">
+          <div class="widget">
+            <span style="font-size: 24px; color: #889D07 !important; text-shadow: rgba(0,0,0,0.1) 0px 1px 1px">Bit</span><span style="font-size: 24px; color: #FA6801 !important; text-shadow: rgba(0,0,0,0.9) 0px 1px 1px">Crunched!</span></p>
+            <address>
+              <strong>BitCrunched!</strong><br>
+              50 N Main St<br>
+              Keyser, WV 26726
+            </address>
+            <p>
+              <i class="icon-phone"></i> (304) 702-1720<br>
+              <i class="icon-envelope-alt"></i> info@BitCrunched.com
+            </p>
+          </div>
+        </div>
+        <div class="span4">
+          <div class="widget">
+            <h5 class="widgetheading">From The Blog</h5>
+
+
+          </div>
+        </div>
+
+        <div class="span3">
+          <div class="widget">
+            <h5 class="widgetheading">Stay updated</h5>
+            <p>
+            Enter your email to subcribe newsletter
+            </p>
+            <form class="subscribe">
+              <div class="input-append">
+                <input type="text" id="appendedInputButton" class="span2">
+                <button type="submit" class="btn btn-primary">Subscribe</button>
+              </div>
+            </form>
+
+
+            <h5 class="widgetheading">Social network</h5>
+            <ul class="social-network">
+              <li><a title="" data-placement="bottom" href="#" data-original-title="Twitter"><i class="icon-twitter-sign icon-2x"></i></a></li>
+              <li><a title="" data-placement="bottom" href="#" data-original-title="Facebook"><i class="icon-facebook-sign icon-2x"></i></a></li>
+              <li><a title="" data-placement="bottom" href="#" data-original-title="Linkedin"><i class="icon-linkedin-sign icon-2x"></i></a></li>
+              <li><a title="" data-placement="bottom" href="#" data-original-title="Pinterest"><i class="icon-pinterest-sign icon-2x"></i></a></li>
+              <li><a title="" data-placement="bottom" href="#" data-original-title="Google plus"><i class="icon-google-plus icon-2x"></i></a></li>
+              <li><a title="" data-placement="bottom" href="#" data-original-title="Behance"><i class="font-icon-social-behance icon-circled active"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </footer>
+  <div class="sub-footer">
+      <div class="container">
+
+
+
+            <p class="span pull-left"><span>&copy;2013 BitCrunched! All right reserved</span></p>
+
+
+
+
+
+            <p class="span pull-right">Designed by <a target="_blank" href="http://www.bitcrunched.com">BitCrunched!</a></p>
+
+
+</div>
+    </div>
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/javascript-combined.js"></script>
+<script>
+      !function ($) {
+        $(function(){
+          // carousel demo
+          $('#myCarousel').carousel()
+        })
+      }(window.jQuery)
+    </script>
+
+    <script type="text/javascript">
+// $(document).ready(function(){
+//  $(window).scroll(function(){
+//   var y = $(window).scrollTop();
+//   if( y > 0 ){
+//       $("#top-shadow").css({'display':'block', 'opacity':y/20});
+//   } else {
+//       $("#top-shadow").css({'display':'block', 'opacity':y/20});
+//   }
+//  });
+// })
+
+//     </script>
+<!--    <script src='js/swipe.js'></script>
+    <script>
+
+    // pure JS
+    var elem = document.getElementById('mySwipe');
+    window.mySwipe = Swipe(elem, {
+      startSlide: 1,
+      auto: 4000,
+      continuous: true,
+      // disableScroll: true,
+      // stopPropagation: true,
+      // callback: function(index, element) {},
+      // transitionEnd: function(index, element) {}
+    });
+
+    // with jQuery
+    // window.mySwipe = $('#mySwipe').Swipe().data('Swipe');
+
+    </script>
+-->
+<script src="js/jquery.flexslider-min.js"></script>
+    <script type="text/javascript" charset="utf-8">
+  // Can also be used with $(document).ready()
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide",
+    direction: "horizontal"
+  });
+});
+</script>
+<script type="text/javascript">
+//   $(window).resize(function(){
+
+//     $('.green-box').css({
+//         position:'absolute',
+//         left: ($(window).width() - $('.green-box').outerWidth())/2,
+//         top: ($(window).height() - $('.green-box').outerHeight())/2
+//     });
+
+// });
+
+// // To initially run the function:
+// $(window).resize();
+</script>
+
+  </body>
+</html>
